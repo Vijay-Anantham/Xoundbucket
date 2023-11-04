c43194f58d4696e6b212beb2ba69fbf1e47ba805
Vijay <vjdev2020@gmail.com>
2023-11-04 20:53:41 +0530

    added test db, made ui to fetch from db, fixed errors

---
+++ lib/domain/domain.dart
@@ -0,0 +1,8 @@
+class Songs{
+  late String name;
+  late String artist;
+  late String album;
+
+  Songs(this.name, this.artist, this.album);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'services/dbfetch.dart';
-
-class Songs {
-  late String name;
-  late String artist;
-  late String album;
-}
+import 'db/Testdb.dart';
+import 'package:xoundbucket/domain/domain.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -16,6 +12,20 @@ void main() {
       body: SongList(),
     ),
   ));
+}
+
+Future<List<Songs>> getsongs() async {
+  final database = JsonDatabase();
+  database.insert({"id": 1, "songs": Songs("abc", "asf", "asg")});
+  database.insert({"id": 2, "songs": Songs("acf", "ajy", "ahng")});
+  database.insert({"id": 3, "songs": Songs("asf", "ahg", "asg")});
+  List<Map<String, dynamic>> deresult = database.selectAll();
+  List<Songs> resultSongs = [Songs("name", "artist", "album")];
+  for (var entry in deresult) {
+    resultSongs.add(entry['songs']);
+  }
+  await Future.delayed(Duration(seconds: 1));
+  return resultSongs;
 }
 
 class SongList extends StatefulWidget {
@@ -27,7 +37,7 @@ class _SongListState extends State<SongList> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Songs>>(
-      future: getSongs(),
+      future: getsongs(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return CircularProgressIndicator();
@@ -36,12 +46,23 @@ class _SongListState extends State<SongList> {
         } else {
           final songs = snapshot.data;
           print("The Value of songs list $songs");
+          if (songs == null) {
+            return ListView.builder(
+              itemCount: (songs != null) ? songs.length : 0,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("Song not exist"),
+                  subtitle: Text("Add songs to your library"),
+                );
+              },
+            );
+          }
           return ListView.builder(
-            itemCount: songs.length,
+            itemCount: (songs != null) ? songs.length : 0,
             itemBuilder: (context, index) {
               return ListTile(
-                title: Text(songs[index].title),
-                subtitle: Text(books[index].author),
+                title: Text(songs[index].name),
+                subtitle: Text(songs[index].artist),
               );
             },
           );
