ff1a0adf9a39a17580ccd0072c1ed88008ccf635
Vijay <vjdev2020@gmail.com>
2023-12-30 08:45:49 +0530

    slight improve on the link account ui

--- docs/workStatus.md
+++ docs/workStatus.md
@@ -11,6 +11,7 @@
     - Login page simple ( to be improved)
     - A test db for fetching songs
     - Connection to spotify using spotify account
+    - Make a make connection page with options
 ## Todo
     - Make friends functionality
         - Friends lister
@@ -18,4 +19,9 @@
     - Profiles page implementation
     - Music platform connections
         - connector for amazon music
-    - Make a make connection page with options
+
+
+
+## Refer to
+- It seems spotify clinet id cannot be provided to the client directly.. Need for registering the application for public use. check the following link
+    - `https://developer.spotify.com/documentation/open-access/concepts`
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -8,6 +8,22 @@ import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:xoundbucket/screens/songList.dart';
 
+// Implements button styling
+ButtonStyle buildButtonStyle() {
+  return ButtonStyle(
+    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+    elevation: MaterialStateProperty.all<double>(8.0),
+    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+      EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+    ),
+    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),
+  );
+}
+
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,17 +41,26 @@ class MyHomePage extends StatelessWidget {
   final String loginApiUrl =
       'http://localhost:3000/'; // Replace with your actual login API endpoint
 
+  // Function that redirect to the internal api call
   Future<void> launchLoginApi(String buttonName, BuildContext context) async {
     try {
-      await launchUrl(Uri.parse(loginApiUrl));
+      if (await canLaunchUrl(Uri.parse(loginApiUrl))) {
+        await launchUrl(Uri.parse(loginApiUrl));
+      } else {
+        print("Url cannot be launched");
+      }
+      // FIXME: Right now we are using an await call but look for a way to make it more event driven
+      await Future.delayed(Duration(seconds: 1));
       chekForward(context);
     } catch (error) {
       print('Error: $error');
     }
   }
 
+  // Function to check if the application page can be transitioned into next window
   Future<void> chekForward(BuildContext context) async {
     final response = await http.get(Uri.parse("http://localhost:3000/state"));
+    // FIXME: This is sometimes causing flow mismatch bad ux sometimes.
     print(response.body);
     if (response.body == "true") {
       Navigator.push(context, MaterialPageRoute(builder: (ctx) => SongList()));
@@ -53,18 +78,35 @@ class MyHomePage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () => launchLoginApi('Spotify', context),
-              child: Text('Spotify'),
+          children: [
+            SizedBox(
+              width: 200.0, // Set a fixed width for all buttons
+              height: 60.0, // Set a fixed height for all buttons
+              child: ElevatedButton(
+                onPressed: () => launchLoginApi('Spotify', context),
+                style: buildButtonStyle(),
+                child: Text('Spotify'),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () => launchLoginApi('Button 2', context),
-              child: Text('Amazon music'),
+            SizedBox(height: 16.0), // Add spacing between buttons
+            SizedBox(
+              width: 200.0, // Set a fixed width for all buttons
+              height: 60.0, // Set a fixed height for all buttons
+              child: ElevatedButton(
+                onPressed: () => launchLoginApi('Button 2', context),
+                style: buildButtonStyle(),
+                child: Text('Amazon Music'),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () => launchLoginApi('Button 3', context),
-              child: Text('Apply music'),
+            SizedBox(height: 16.0), // Add spacing between buttons
+            SizedBox(
+              width: 200.0, // Set a fixed width for all buttons
+              height: 60.0, // Set a fixed height for all buttons
+              child: ElevatedButton(
+                onPressed: () => launchLoginApi('Button 3', context),
+                style: buildButtonStyle(),
+                child: Text('Apple Music'),
+              ),
             ),
           ],
         ),
