56c18730ef00018e4e3a9b1e1f6ec2657224563a
Vijay <vjdev2020@gmail.com>
2024-01-04 15:42:55 +0530

    added mongodb atlas connector

--- backend/connectors/spotifyconnect.js
+++ backend/connectors/spotifyconnect.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const config = require('./config.json')
+const config = require('../config.json')
 const SpotifyWebApi = require('spotify-web-api-node');
 const cors = require('cors')
 
